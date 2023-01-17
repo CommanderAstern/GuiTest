61b27d8a517000b7bae60ea99d3f1040d601291d
abhiraj-mengade <i2am@yandex.com>
2023-01-17 23:14:50 +0530

    Add IoT pages

--- piTest/simpleApp/urls.py
+++ piTest/simpleApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('select', views.select, name='select'),
+    path('scan', views.scan, name='scan'),
+    path('battery', views.battery, name='battery'),
+    path('batteries', views.batteries, name='batteries'),
 ]
--- piTest/simpleApp/views.py
+++ piTest/simpleApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 # Create your views here.
 
@@ -8,10 +8,10 @@ def index(request):
     if 'counter' in request.session:
         request.session['counter'] += 1
         val = request.session['counter']
-        GPIO.output(7,True) if val%2 else GPIO.output(7,False) 
+        # GPIO.output(7,True) if val%2 else GPIO.output(7,False) 
     else:
         request.session['counter'] = 1
-        GPIO.output(7,True)
+        # GPIO.output(7,True)
 
     return render(request, 'simpleApp/index.html', {'counter': request.session['counter']})
     # if request.method == "POST":
@@ -22,3 +22,25 @@ def index(request):
     # else :
     #   request.session['count'] = 0
     #   return render(request, 'simpleApp/index.html')
+
+def select(request):
+    if request.method == "POST":
+        try:
+            request.session['counter'] +=1
+        except:
+            request.session['counter'] = 0
+    else :
+        request.session['counter'] = 0
+        return render(request, 'simpleApp/select.html')
+
+    return render(request, 'simpleApp/select.html', {'counter': request.session['counter']})
+
+def scan(request):
+
+    return render(request, 'simpleApp/scan.html', {'scanned': True})
+
+def battery(request):
+    return render(request, 'simpleApp/battery.html')
+
+def batteries(request):
+    return render(request, 'simpleApp/batteries.html')
